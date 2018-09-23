a907f06797c62f31f5163765b18f16628267c2ad
taylortaurus <taylortaurus0517@gmail.com>
2018-09-23 08:29:07 +0800

    update url

--- src/data/dialog.js
+++ src/data/dialog.js
@@ -13,7 +13,7 @@ export default {
             details: [
                 "欢迎来到我的站点，<br>",
                 "很高兴在这里遇见你！",
-                '<a href="https://xlab.taylortaurus.top/" target="_blank"> <img src = "./src/img/skr.jpg" /></a>'
+                '<a href="https://xlab.taylortaurus.top/coderain/" target="_blank"> <img src = "./src/img/skr.jpg" /></a>'
             ],
             responses: [
                 {
